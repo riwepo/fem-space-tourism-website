41889dca0d0aec9afda2e71abda2e933cf78afe4
Richard Potter <richardpotter.au@gmail.com>
2023-08-13 07:47:04 +1000

    implement numbered title locally

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import classes from "./NavItem.module.css";
+
 function NavItem({ className, item, isActive }) {
   return (
     <p
       className={`${className} uppercase text-light fs-300 ff-sans-cond letter-spacing-2 ${
-        isActive ? "underline-indicator-active" : undefined
-      }`}
+        classes.numberedTitle
+      } ${isActive ? "underline-indicator-active" : undefined}`}
     >
       <span>{item.id}</span> {item.label}
     </p>
