da81e5b30f8ad9be3087feac5ea4fcad1759e541
Richard Potter <richardpotter.au@gmail.com>
2023-08-13 15:56:21 +1000

    added skeleton destination page

--- src/pages/DestinationPage.js
+++ src/pages/DestinationPage.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+import Destination from "../components/destination/Destination";
+
+import imgMoon from "../assets/destination/image-moon.png";
+
+import classes from "./DestinationPage.module.css";
+import centeredGrid from "../shared_css/centered-grid.module.css";
+
 function DestinationPage() {
-  return <p>Destination Page</p>;
+  return (
+    <div className={centeredGrid.centeredGrid}>
+      <div>
+        <h2
+          className={`ff-sans-cond fs-500 upper-case letter-spacing-1 text-accent ${classes.h1}`}
+        >
+          Pick your destination
+        </h2>
+        <img src={imgMoon} alt="moon" />
+      </div>
+      <div>
+        <Destination />
+      </div>
+    </div>
+  );
 }
 
 export default DestinationPage;
