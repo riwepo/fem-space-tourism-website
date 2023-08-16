79375ed8010a2399d7d1d11eb44315cbe43ebcf7
Richard Potter <richardpotter.au@gmail.com>
2023-08-16 11:33:36 +1000

    set background for destination

--- src/pages/DestinationLayout.js
+++ src/pages/DestinationLayout.js
@@ -5,18 +5,22 @@ import DestinationNavigation from "../components/DestinationNavigation";
 
 import "./DestinationLayout.css";
 import "../shared_css/centered-grid.css";
+import "../shared_css/background.css";
 
 function DestinationLayout() {
   return (
-    <div className="destination-layout centered-grid">
-      <h1 className="ff-sans-con fs-500 upper-case letter-spacing-1 centered-grid-c23">
-        <span>01</span> Pick your destination
-      </h1>
-      <DestinationNavigation className="navigation" />
-      <section className="section">
-        <Outlet />
-      </section>
-    </div>
+    <>
+      <div className="background background--destination"></div>
+      <div className="destination-layout centered-grid">
+        <h1 className="ff-sans-con fs-500 upper-case letter-spacing-1 centered-grid-c23">
+          <span>01</span> Pick your destination
+        </h1>
+        <DestinationNavigation className="navigation" />
+        <section className="section">
+          <Outlet />
+        </section>
+      </div>
+    </>
   );
 }
 
