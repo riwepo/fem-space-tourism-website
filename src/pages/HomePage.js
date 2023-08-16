90f3d07c60850890c287d116871a5abc3370fe8c
Richard Potter <richardpotter.au@gmail.com>
2023-08-16 11:11:28 +1000

    change background for each page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,25 +7,28 @@ import "../shared_css/centered-grid.css";
 
 function HomePage() {
   return (
-    <div className="home-page centered-grid">
-      <div className="centered-grid-c2">
-        <h1 className="ff-sans-cond fs-500 upper-case letter-spacing-1 text-accent">
-          So you want to travel to
-          <span className="ff-serif fs-900 upper-case text-light d-block">
-            Space
-          </span>
-        </h1>
-        <p className="text-accent">
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
+    <>
+      <div className="background"></div>
+      <div className="home-page centered-grid">
+        <div className="centered-grid-c2">
+          <h1 className="ff-sans-cond fs-500 upper-case letter-spacing-1 text-accent">
+            So you want to travel to
+            <span className="ff-serif fs-900 upper-case text-light d-block">
+              Space
+            </span>
+          </h1>
+          <p className="text-accent">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="centered-grid-c3">
+          <LargeButton className="button" />
+        </div>
       </div>
-      <div className="centered-grid-c3">
-        <LargeButton className="button" />
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -3,9 +3,6 @@ import { Outlet } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
-// import classes from "./RootLayout.module.css";
-import "./RootLayout.css";
-
 function RootLayout() {
   return (
     <div className="root-layout">
