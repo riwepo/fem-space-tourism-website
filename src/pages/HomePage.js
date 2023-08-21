6526109257263359b168127a1694cbaa514ff9b1
Richard Potter <richardpotter.au@gmail.com>
2023-08-22 08:26:49 +1000

    move home page from root layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,31 +3,29 @@ import React from "react";
 import LargeButton from "../components/LargeButton";
 
 import "../css/utils/background.css";
-import "../css/utils/centered-grid.css";
+import "../css/utils/grid-container.css";
 import "./HomePage.css";
 
 function HomePage() {
   return (
     <>
       <div className="background background--home"></div>
-      <div className="home-page centered-grid">
-        <div className="centered-grid-c2">
-          <h1 className="ff-sans-cond fs-500 uppercase letter-spacing-1 text-accent">
-            So you want to travel to
-            <span className="ff-serif fs-900 uppercase text-light d-block">
+      <div className="grid-container grid-container--home">
+        <div>
+          <h1 className="ff-sans-cond fs-500 text-accent uppercase letter-spacing-1">
+            So, you want to travel to{" "}
+            <span className="ff-serif fs-900 text-light uppercase d-block">
               Space
             </span>
           </h1>
-          <p className="text-accent">
+          <p>
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
+            world experience! Explore
           </p>
         </div>
-        <div className="centered-grid-c3">
-          <LargeButton className="button" />
-        </div>
+        <LargeButton className="large-button" />
       </div>
     </>
   );
--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
-// import MainNavigation from "../components/MainNavigation";
 import PrimaryHeader from "../components/PrimaryHeader";
-import LargeButton from "../components/LargeButton";
 
-import "../css/utils/centered-grid.css";
 import "./RootLayout.css";
 
 const getTitle = (path) => {
@@ -26,23 +23,10 @@ function RootLayout() {
   return (
     <div className="root-layout">
       <PrimaryHeader />
-      <div className="grid-container grid-container--home">
-        <div>
-          <h1 className="ff-sans-cond fs-500 text-accent uppercase letter-spacing-1">
-            So, you want to travel to{" "}
-            <span className="ff-serif fs-900 text-light uppercase d-block">
-              Space
-            </span>
-          </h1>
-          <p>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience! Explore
-          </p>
-        </div>
-        <LargeButton className="large-button" />
-      </div>
+      <main>
+        <Outlet />
+      </main>
+
       {/* <main>
         <div
           className={`centered-grid title-container ${
