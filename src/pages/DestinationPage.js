835a8e66797ae80a45fae050cf183abf3afe0fbd
Richard Potter <richardpotter.au@gmail.com>
2023-08-23 09:44:33 +1000

    implement

--- src/pages/DestinationPage.js
+++ src/pages/DestinationPage.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+
+import TabList from "../components/TabList";
 
 import { getImagePath } from "../utils";
 
-import "../css/utils/centered-grid.css";
+import "../css/utils/numbered-title.css";
 import "./DestinationPage.css";
 
 const destinationsData = [
@@ -48,46 +49,88 @@ const getMatchingDestination = (data, name) => {
 };
 
 function DestinationPage() {
-  const params = useParams();
-  const destinationId = params.id;
+  //const params = useParams();
+  const destinationId = "moon"; // params.id;
   const destinationData = getMatchingDestination(
     destinationsData,
     destinationId
   );
   const imagePath = getImagePath("destination", destinationId);
   return (
-    <div className="destination-page centered-grid">
-      <img
-        src={imagePath}
-        alt={destinationData.name}
-        className="centered-grid-c2"
-      />
-      <div className="centered-grid-c3">
-        <h2 className="ff-serif uppercase fs-800">{destinationData.name}</h2>
-        <p className="ff-sans-normal fs-400 text-accent">
-          {destinationData.description}
-        </p>
-        <div className="details flex">
-          <div>
-            <p className="ff-serif-cond fs-200 text-accent uppercase letter-spacing-3">
-              Avg. distance
-            </p>
-            <p className="ff-serif fs-500 uppercase">
-              {destinationData.distance}
-            </p>
+    <>
+      <div className="background background--destination"></div>
+      <div className="grid-container grid-container--destination flow">
+        <h1 className="numbered-title">
+          <span aria-hidden="true">01</span> Pick your destination
+        </h1>
+        <img src={imagePath} alt="the moon"></img>
+        <TabList />
+        <article className="destination-info">
+          <h2 className="ff-serif fs-800 uppercase">{destinationData.name}</h2>
+          <p>{destinationData.description}</p>
+          <div className="destination-meta flex">
+            <div>
+              <h3 className="fs-200 uppercase text-accent">Avg. distance</h3>
+              <p className="ff-serif uppercase">{destinationData.distance}</p>
+            </div>
+            <div>
+              <h3 className="fs-200 uppercase text-accent">Est. travel time</h3>
+              <p className="ff-serif uppercase">{destinationData.travel}</p>
+            </div>
           </div>
-          <div>
-            <p className="ff-serif-cond fs-200 text-accent uppercase letter-spacing-3">
-              Est. travel time
-            </p>
-            <p className="ff-serif fs-500 uppercase">
-              {destinationData.travel}
-            </p>
-          </div>
+        </article>
+
+        {/* <div>
+          <h1 className="ff-sans-cond fs-500 text-accent uppercase letter-spacing-1">
+            So, you want to travel to{" "}
+            <span className="ff-serif fs-900 text-light uppercase d-block">
+              Space
+            </span>
+          </h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience! Explore
+          </p>
         </div>
+        <LargeButton className="large-button" /> */}
       </div>
-    </div>
+    </>
   );
+  // return (
+  //   <div className="destination-page centered-grid">
+  //     <img
+  //       src={imagePath}
+  //       alt={destinationData.name}
+  //       className="centered-grid-c2"
+  //     />
+  //     <div className="centered-grid-c3">
+  //       <h2 className="ff-serif uppercase fs-800">{destinationData.name}</h2>
+  //       <p className="ff-sans-normal fs-400 text-accent">
+  //         {destinationData.description}
+  //       </p>
+  //       <div className="details flex">
+  //         <div>
+  //           <p className="ff-serif-cond fs-200 text-accent uppercase letter-spacing-3">
+  //             Avg. distance
+  //           </p>
+  //           <p className="ff-serif fs-500 uppercase">
+  //             {destinationData.distance}
+  //           </p>
+  //         </div>
+  //         <div>
+  //           <p className="ff-serif-cond fs-200 text-accent uppercase letter-spacing-3">
+  //             Est. travel time
+  //           </p>
+  //           <p className="ff-serif fs-500 uppercase">
+  //             {destinationData.travel}
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default DestinationPage;
