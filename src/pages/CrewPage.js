1a0b8bec991fbeb131b05b05f4122aa965c55fb4
Richard Potter <richardpotter.au@gmail.com>
2023-08-24 09:10:37 +1000

    isActive not used

--- src/pages/CrewPage.js
+++ src/pages/CrewPage.js
@@ -12,28 +12,24 @@ const crewData = [
     name: "Douglas Hurley",
     role: "Commander",
     bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
-    isActive: true,
   },
   {
     id: "2",
     name: "Mark Shuttleworth",
     role: "Mission Specialist",
     bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
-    isActive: false,
   },
   {
     id: "3",
     name: "Victor Glover",
     role: "Pilot",
     bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
-    isActive: false,
   },
   {
     id: "4",
     name: "Anousheh Ansari",
     role: "Flight Engineer",
     bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
-    isActive: false,
   },
 ];
 
@@ -57,7 +53,7 @@ function CrewPage() {
       <div className="background background--crew"></div>
       <div className="grid-container grid-container--crew flow">
         <h1 className="numbered-title">
-          <span aria-hidden="true">01</span> Meet your crew
+          <span aria-hidden="true">03</span> Meet your crew
         </h1>
         <DotIndicators
           items={crewData}
