f7f1b45100c29bb256c86f29e824e97f39fdb794
Richard Potter <richardpotter.au@gmail.com>
2023-08-23 12:26:21 +1000

    in progress

--- src/pages/CrewPage.js
+++ src/pages/CrewPage.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
-import CrewNavigation from "../components/CrewNavigation";
+import DotIndicators from "../components/DotIndicators";
 
 import { getImagePath } from "../utils";
 
-import "../css/utils/centered-grid.css";
 import "./CrewPage.css";
 
 const crewData = [
@@ -14,24 +12,28 @@ const crewData = [
     name: "Douglas Hurley",
     role: "Commander",
     bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+    isActive: true,
   },
   {
     id: "02",
     name: "Mark Shuttleworth",
     role: "Mission Specialist",
     bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+    isActive: false,
   },
   {
     id: "03",
     name: "Victor Glover",
     role: "Pilot",
     bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
+    isActive: false,
   },
   {
     id: "04",
     name: "Anousheh Ansari",
     role: "Flight Engineer",
     bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+    isActive: false,
   },
 ];
 
@@ -52,30 +54,51 @@ const getImageKeyFromName = (name) => {
 };
 
 function CrewPage() {
-  const params = useParams();
-  const crewMemberId = params.id;
+  const crewMemberId = "01";
   const crewMemberData = getMatchingCrewMember(crewData, crewMemberId);
   const imageKey = getImageKeyFromName(crewMemberData.name);
   const imagePath = getImagePath("crew", imageKey);
   return (
-    <div className="crew-page centered-grid">
-      <div className="content centered-grid-c2">
-        <h2 className="ff-serif fs-700 text-light uppercase">
-          <span className="fs-600 d-block">{crewMemberData.role}</span>{" "}
-          {crewMemberData.name}
-        </h2>
-        <p className="ff-sans-normal fs-400 text-accent">
-          {crewMemberData.bio}
-        </p>
+    <>
+      <div className="background background--crew"></div>
+      <div className="grid-container-xxxx grid-container--crew flow">
+        <h1 className="numbered-title">
+          <span aria-hidden="true">01</span> Meet your crew
+        </h1>
+        <DotIndicators items={crewData} />
+        <article className="crew-details">
+          <h2 className="fs-600 ff-serif uppercase">Commander</h2>
+          <p className="fs-700 uppercase ff-serif">Douglas Hurley</p>
+          <p>
+            Douglas Gerald Hurley is an American engineer, former Marine Corps
+            pilot and former NASA astronaut. He launched into space for the
+            third time as commander of Crew Dragon Demo-2.
+          </p>
+        </article>
+        <img src={imagePath} alt="crew member"></img>
       </div>
-      <CrewNavigation className="navigation centered-grid-c2" />
-      <img
-        src={imagePath}
-        alt={crewMemberData.name}
-        className="centered-grid-c3"
-      />
-    </div>
+    </>
   );
+
+  // return (
+  //   <div className="crew-page centered-grid">
+  //     <div className="content centered-grid-c2">
+  //       <h2 className="ff-serif fs-700 text-light uppercase">
+  //         <span className="fs-600 d-block">{crewMemberData.role}</span>{" "}
+  //         {crewMemberData.name}
+  //       </h2>
+  //       <p className="ff-sans-normal fs-400 text-accent">
+  //         {crewMemberData.bio}
+  //       </p>
+  //     </div>
+  //     <CrewNavigation className="navigation centered-grid-c2" />
+  //     <img
+  //       src={imagePath}
+  //       alt={crewMemberData.name}
+  //       className="centered-grid-c3"
+  //     />
+  //   </div>
+  // );
 }
 
 export default CrewPage;
