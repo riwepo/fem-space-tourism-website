cc94429a09ae18ba45f48c06a71252df0e5a9846
Richard Potter <richardpotter.au@gmail.com>
2023-08-23 12:25:23 +1000

    make data driven

--- src/components/DotIndicators.js
+++ src/components/DotIndicators.js
@@ -2,18 +2,16 @@ import React from "react";
 
 import "./DotIndicators.css";
 
-function DotIndicators() {
+function DotIndicators({ items }) {
   return (
     <div className="dot-indicators flex">
-      <button aria-selected="true">
-        <span className="sr-only">Slide title</span>
-      </button>
-      <button aria-selected="false">
-        <span className="sr-only">Slide title</span>
-      </button>
-      <button aria-selected="false">
-        <span className="sr-only">Slide title</span>
-      </button>
+      {items.map((item) => {
+        return (
+          <button key={item.id} aria-selected={item.isActive}>
+            <span className="sr-only">The {item.role}</span>
+          </button>
+        );
+      })}
     </div>
   );
 }
