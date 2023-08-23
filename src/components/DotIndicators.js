606064023adaed4ad69416407aaec18394964d32
Richard Potter <richardpotter.au@gmail.com>
2023-08-23 15:50:30 +1000

    implement dot clicks

--- src/components/DotIndicators.js
+++ src/components/DotIndicators.js
@@ -2,12 +2,21 @@ import React from "react";
 
 import "./DotIndicators.css";
 
-function DotIndicators({ items }) {
+function DotIndicators({ items, activeItemId, onItemChange }) {
+  const clickHandler = (event) => {
+    const id = event.target.dataset.id;
+    onItemChange(id);
+  };
   return (
     <div className="dot-indicators flex">
       {items.map((item) => {
         return (
-          <button key={item.id} aria-selected={item.isActive}>
+          <button
+            key={item.id}
+            aria-selected={item.id === activeItemId}
+            data-id={item.id}
+            onClick={clickHandler}
+          >
             <span className="sr-only">The {item.role}</span>
           </button>
         );
--- src/pages/CrewPage.js
+++ src/pages/CrewPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import DotIndicators from "../components/DotIndicators";
 
@@ -8,28 +8,28 @@ import "./CrewPage.css";
 
 const crewData = [
   {
-    id: "01",
+    id: "1",
     name: "Douglas Hurley",
     role: "Commander",
     bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
     isActive: true,
   },
   {
-    id: "02",
+    id: "2",
     name: "Mark Shuttleworth",
     role: "Mission Specialist",
     bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
     isActive: false,
   },
   {
-    id: "03",
+    id: "3",
     name: "Victor Glover",
     role: "Pilot",
     bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
     isActive: false,
   },
   {
-    id: "04",
+    id: "4",
     name: "Anousheh Ansari",
     role: "Flight Engineer",
     bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
@@ -54,10 +54,14 @@ const getImageKeyFromName = (name) => {
 };
 
 function CrewPage() {
-  const crewMemberId = "01";
-  const crewMemberData = getMatchingCrewMember(crewData, crewMemberId);
+  const [activeItemId, setActiveItemId] = useState("1");
+  const crewMemberData = getMatchingCrewMember(crewData, activeItemId);
   const imageKey = getImageKeyFromName(crewMemberData.name);
   const imagePaths = getImagePaths("crew", imageKey);
+
+  const itemChangeHandler = (id) => {
+    setActiveItemId(id);
+  };
   return (
     <>
       <div className="background background--crew"></div>
@@ -65,17 +69,17 @@ function CrewPage() {
         <h1 className="numbered-title">
           <span aria-hidden="true">01</span> Meet your crew
         </h1>
-        <DotIndicators items={crewData} />
+        <DotIndicators
+          items={crewData}
+          activeItemId={activeItemId}
+          onItemChange={itemChangeHandler}
+        />
         <article className="crew-details flow">
           <header className="flow flow--space-small">
-            <h2 className="fs-600 ff-serif uppercase">Commander</h2>
-            <p className="fs-700 uppercase ff-serif">Douglas Hurley</p>
+            <h2 className="fs-600 ff-serif uppercase">{crewMemberData.role}</h2>
+            <p className="fs-700 uppercase ff-serif">{crewMemberData.name}</p>
           </header>
-          <p>
-            Douglas Gerald Hurley is an American engineer, former Marine Corps
-            pilot and former NASA astronaut. He launched into space for the
-            third time as commander of Crew Dragon Demo-2.
-          </p>
+          <p>{crewMemberData.bio}</p>
         </article>
         <picture>
           <source srcSet={imagePaths.webp} type="image/webp" />
