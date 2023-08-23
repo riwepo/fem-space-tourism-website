2a4c0502858bebf5f505614a8cb39a968d161626
Richard Potter <richardpotter.au@gmail.com>
2023-08-23 15:50:19 +1000

    make ids start 1

--- src/pages/DestinationPage.js
+++ src/pages/DestinationPage.js
@@ -9,7 +9,7 @@ import "./DestinationPage.css";
 
 const destinationsData = [
   {
-    id: "0",
+    id: "1",
     name: "Moon",
     description:
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
@@ -17,7 +17,7 @@ const destinationsData = [
     travel: "3 days",
   },
   {
-    id: "1",
+    id: "2",
     name: "Mars",
     description:
       "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
@@ -25,7 +25,7 @@ const destinationsData = [
     travel: "9 months",
   },
   {
-    id: "2",
+    id: "3",
     name: "Europa",
     description:
       "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
@@ -33,7 +33,7 @@ const destinationsData = [
     travel: "3 years",
   },
   {
-    id: "3",
+    id: "4",
     name: "Titan",
     description:
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
@@ -53,7 +53,7 @@ const getMatchingDestination = (data, id) => {
 };
 
 function DestinationPage() {
-  const [activeTabId, setActiveTabId] = useState("0");
+  const [activeTabId, setActiveTabId] = useState("1");
   const destinationData = getMatchingDestination(destinationsData, activeTabId);
   const imageKey = destinationData.name.toLowerCase();
   const imagePaths = getImagePaths("destination", imageKey);
