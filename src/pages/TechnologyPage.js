6565ab6947b528948934382272cc69565a5787d2
Richard Potter <richardpotter.au@gmail.com>
2023-08-24 09:11:09 +1000

    begin implement

--- src/pages/TechnologyPage.js
+++ src/pages/TechnologyPage.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+
+import DotIndicators from "../components/DotIndicators";
+
+import { getImagePaths } from "../utils";
+
+import "./TechnologyPage.css";
+
+const techData = [
+  {
+    id: "1",
+    name: "Launch vehicle",
+    description:
+      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+  },
+  {
+    id: "2",
+    name: "Spaceport",
+    description:
+      "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+  },
+  {
+    id: "3",
+    name: "Space capsule",
+    description:
+      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+  },
+];
+
+const getImageKeyFromName = (name) => {
+  const lowerCase = name.toLowerCase();
+  const spacesToDash = lowerCase.replace(" ", "-");
+  return spacesToDash;
+};
 
 function TechnologyPage() {
-  return <p>Technology Page</p>;
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const techItemData = techData[activeItemIndex];
+  const imageKey = getImageKeyFromName(techItemData.name);
+  const imagePaths = getImagePaths("tech", imageKey);
+
+  const indexChangeHandler = (index) => {
+    setActiveItemIndex(index);
+  };
+  return (
+    <>
+      <div className="background background--tech"></div>
+      <div className="grid-container grid-container--tech flow">
+        <h1 className="numbered-title">
+          <span aria-hidden="true">03</span> Space launch 101
+        </h1>
+        <picture>
+          <source srcSet={imagePaths.webp} type="image/webp" />
+          <img src={imagePaths.png} alt="tech member"></img>
+        </picture>
+        <DotIndicators
+          items={techData}
+          activeItemIndex={activeItemIndex}
+          onIndexChange={indexChangeHandler}
+        />
+        <article className="tech-details flow">
+          <header className="flow flow--space-small">
+            <h2 className="fs-600 ff-serif uppercase">The terminology...</h2>
+            <p className="fs-700 uppercase ff-serif">{techItemData.name}</p>
+          </header>
+          <p>{techItemData.description}</p>
+        </article>
+      </div>
+    </>
+  );
 }
 
 export default TechnologyPage;
