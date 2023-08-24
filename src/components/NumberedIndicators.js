b646f2b08748f4510b1973da7f954a4c15f957a9
Richard Potter <richardpotter.au@gmail.com>
2023-08-24 10:14:08 +1000

    add click and key

--- src/components/NumberedIndicators.js
+++ src/components/NumberedIndicators.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 import "./NumberedIndicators.css";
 
 function NumberedIndicators({ items, activeItemIndex, onIndexChange }) {
+  const buttonRefs = useRef([]);
+  useEffect(() => {
+    buttonRefs.current[activeItemIndex].focus();
+  }, [activeItemIndex]);
+  const clickHandler = (event) => {
+    const index = +event.target.dataset.itemIndex;
+    onIndexChange(index);
+  };
+  const keyDownHandler = (event) => {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    if (event.keyCode === LEFT_KEY && activeItemIndex > 0) {
+      onIndexChange(activeItemIndex - 1);
+    } else if (
+      event.keyCode === RIGHT_KEY &&
+      activeItemIndex < items.length - 1
+    ) {
+      onIndexChange(activeItemIndex + 1);
+    }
+  };
   return (
-    <div className="numbered-indicators grid">
+    <div className="numbered-indicators flex" onKeyDown={keyDownHandler}>
       {items.map((item, index) => {
         return (
-          <button aria-selected={index === activeItemIndex} key={item.id}>
+          <button
+            aria-selected={index === activeItemIndex}
+            key={item.id}
+            onClick={clickHandler}
+            data-item-index={index}
+            ref={(element) => (buttonRefs.current[index] = element)}
+          >
             <span className="sr-only">{item.name}</span>
             {index + 1}
           </button>
